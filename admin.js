6dade67913d5cd41ffc1ee5e37a1d4e649a6d7a9
Prime267-hub <thegodfath3r547@gmail.com>
2026-01-17 02:02:10 +0200

    Update admin.js

--- admin.js
+++ admin.js
@@ -9,18 +9,14 @@ const carList = document.getElementById("admin-car-list");
 
 // Render cars
 function renderCars() {
-  carList.innerHTML = "";
-  cars.forEach((car, index) => {
-    const div = document.createElement("div");
-    div.className = "admin-car-card";
-    div.innerHTML = `
-      <p><strong>${car.make} ${car.model}</strong></p>
-      <p>Price: N$ ${car.price}</p>
-      <p>Year: ${car.year}</p>
-      <p>Mileage: ${car.mileage} km</p>
-      <p>City: ${car.city}</p>
-      <button onclick="deleteCar(${index})">Delete</button>
-    `;
+   <img src="${car.image || 'https://via.placeholder.com/300'}" class="admin-car-image">
+  <p><strong>${car.make} ${car.model}</strong></p>
+  <p>Price: N$ ${car.price}</p>
+  <p>Year: ${car.year}</p>
+  <p>Mileage: ${car.mileage} km</p>
+  <p>City: ${car.city}</p>
+  <button onclick="deleteCar(${index})">Delete</button>
+`;
     carList.appendChild(div);
   });
 }
@@ -31,7 +27,9 @@ form.addEventListener("submit", function(e) {
   const newCar = {
     make: document.getElementById("make").value,
     model: document.getElementById("model").value,
-    price: Number(document.getElementById("price").value),
+    pridiv.innerHTML = `
+  
+ce: Number(document.getElementById("price").value),
     year: Number(document.getElementById("year").value),
     mileage: Number(document.getElementById("mileage").value),
     city: document.getElementById("city").value,
